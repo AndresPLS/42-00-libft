9aaf06ec88f2e3e1d40d770258d02c8e4fd1a464
Andrés Polo <andrespolo13@gmail.com>
2024-09-30 19:16:05 +0200

    upload overload

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,7 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apolo-lo <apolo-lo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 18:04:37 by apolo-lo          #+#    #+#             */
+/*   Updated: 2024/09/30 19:09:07 by apolo-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdio.h>
 
-char *ft_strchr(const char *s, int c)
+char    *ft_strchr(const char *s, int c)
 {
     unsigned int i;
 
