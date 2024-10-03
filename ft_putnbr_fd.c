1c422565ad5fe5d5914d39b1aade1d94c5ed3973
Andres Polo <andrespolo13@gmail.com>
2024-10-03 12:01:22 +0200

    upload putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,20 +6,28 @@
 /*   By: apolo-lo <apolo-lo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 17:56:27 by apolo-lo          #+#    #+#             */
-/*   Updated: 2024/10/03 10:43:39 by apolo-lo         ###   ########.fr       */
+/*   Updated: 2024/10/03 12:01:01 by apolo-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putnbr_fd(int b, int fd)
+void ft_putnbr_fd(int n, int fd)
 {
+    int digit;
+
     if (n == -2147483648)
+    {
         write(fd, "-2147483648", 11);
-    else if (n < 0)
-    {
+        return;
     }
-    else
+    if (n < 0)
     {
+        write(fd, "-", 1);
+        n = -n;
     }
+    if (n > 9)
+        ft_putnbr_fd(n / 10, fd);
+    digit = n % 10 + '0';
+    write(fd, &digit, 1);
 }
