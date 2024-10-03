78a2d9575aef64d97dfea2daa80943be81497409
Andres <andrespolo13@gmail.com>
2024-10-03 18:59:41 +0200

    upload fine changes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: apolo-lo <apolo-lo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 20:41:48 by apolo-lo          #+#    #+#             */
-/*   Updated: 2024/10/03 18:55:48 by apolo-lo         ###   ########.fr       */
+/*   Updated: 2024/10/03 18:59:24 by apolo-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 // char **ft_split(char const *s, char c);
 // char *ft_itoa(int n);
-// char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 // void ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
