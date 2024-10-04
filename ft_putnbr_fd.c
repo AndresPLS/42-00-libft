c9c274763773174f454a37473d81b0ec1dc1ec0c
Andres Polo <andrespolo13@gmail.com>
2024-10-04 10:52:02 +0200

    upload changes date
    Error date

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,29 +6,29 @@
 /*   By: apolo-lo <apolo-lo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 17:56:27 by apolo-lo          #+#    #+#             */
-/*   Updated: 2024/10/03 18:21:43 by apolo-lo         ###   ########.fr       */
+/*   Updated: 2024/10/04 11:16:46 by apolo-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+void ft_putnbr_fd(int n, int fd)
 {
-	int	digit;
+    int digit;
 
-	if (n == -2147483648)
-	{
-		write (fd, "-2147483648", 11);
-		return ;
-	}
-	if (n < 0)
-	{
-		write (fd, "-", 1);
-		n = -n;
-	}
-	if (n > 9)
-		ft_putnbr_fd (n / 10, fd);
-	digit = n % 10 + '0';
-	write (fd, &digit, 1);
+    if (n == -2147483648)
+    {
+        write(fd, "-2147483648", 11);
+        return;
+    }
+    if (n < 0)
+    {
+        write(fd, "-", 1);
+        n = -n;
+    }
+    if (n > 9)
+        ft_putnbr_fd(n / 10, fd);
+    digit = n % 10 + '0';
+    write(fd, &digit, 1);
 }
