0cc39128044dbc52701794db551d247951701a23
Andres <andrespolo13@gmail.com>
2024-10-07 17:24:13 +0200

    upload changes

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: apolo-lo <apolo-lo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 14:55:57 by apolo-lo          #+#    #+#             */
-/*   Updated: 2024/10/03 18:55:53 by apolo-lo         ###   ########.fr       */
+/*   Updated: 2024/10/07 17:16:28 by apolo-lo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ static char	*to_strclean(const char *s1, size_t start, size_t len)
 	size_t	i;
 
 	if (len <= 0 || start >= ft_strlen(s1))
-		return (NULL);
+		return (ft_calloc(1, sizeof(char)));
 	str = ft_calloc(len + 1, sizeof(char));
 	if (!str)
 		return (NULL);
@@ -52,9 +52,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	start;
 	size_t	end;
 
+	if (!s1 || !set)
+		return (NULL);
 	start = 0;
 	end = ft_strlen(s1) - 1;
-	if (!s1 || !set || ft_strlen(s1) == 0)
+	if (ft_strlen(s1) == 0)
 		return (ft_strdup(""));
 	while (to_cut(set, s1[start]))
 		start++;
@@ -62,15 +64,3 @@ char	*ft_strtrim(char const *s1, char const *set)
 		end--;
 	return (to_strclean(s1, start, end - (start - 1)));
 }
-
-/* 
-int	 main ()
-{
-	char *str = "xñxñxññxx joder ñxñxñxñx";
-
-	char *set = ft_strtrim(str, "xñ ");
-
-	printf("%s", set);
-
-	return 0;
-} */
